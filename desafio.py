39d3de3c94d6e62fb75a4a63d6e3dd3a5296ed52
Viviane Jesus <vivianejesuscontato@gmail.com>
2024-11-12 21:31:12 -0300

    loja

---
+++ desafio.py
@@ -0,0 +1,4 @@
+print("desafio do caique")
+a=145
+b=234
+print(a+b)
---
+++ lojas.py
@@ -0,0 +1,8 @@
+
+cliente=("Brayan")
+mes=("Julho")
+valor=128.00
+desconto=56
+cupom=(cliente+"É56")
+
+print("Olá " + cliente + ", em " + mes + " você realizou uma compra no valor de R$" , float(valor) , " e ganhou um desconto de " , int(desconto) , "% em sua próxima compra. Use o cupom " + cupom)
